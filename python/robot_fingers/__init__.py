9c94d5091632ac9a7a1ed0c962b8d22880e30d67
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-02-27 15:06:24 +0100

    Add python module from blmc_robots

---
+++ python/robot_fingers/__init__.py
@@ -0,0 +1,6 @@
+from blmc_robots.robot import Robot, demo_print_position
+
+from blmc_robots.py_real_finger import *
+from blmc_robots.py_trifinger import *
+from blmc_robots.py_one_joint import *
+from blmc_robots.py_two_joint import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+        packages=['robot_fingers'],
+        package_dir={'': 'python'}
+)
+
+setup(**d)
