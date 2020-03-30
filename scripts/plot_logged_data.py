e2ce6cd2eead3eb96c19100fbaac5435ea86fef9
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-03-30 15:58:40 +0200

    Extend plotting script to work with column names
    Load the data file with pandas instead of numpy.  Pandas can read the
    column header line and allows easy plotting based on the name instead of
    the column index (index also still works, though).
    
    For example, to plot the observed position of the first three joints run
    
        plot_logged_data.py file.log observation_position_0 \
            observation_position_1 observation_position_2

--- scripts/plot_logged_data.py
+++ scripts/plot_logged_data.py
@@ -2,23 +2,27 @@
 """Simply script to quickly plot data from a log file."""
 
 import argparse
-import numpy as np
+import pandas
 import matplotlib.pyplot as plt
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("filename", type=str)
-    parser.add_argument("column_index", type=int, nargs="+")
-    parser.add_argument("--base_index", type=int, default=0)
+    parser.add_argument("columns", type=str, nargs="+")
+    parser.add_argument("--base", type=str, default="0")
     args = parser.parse_args()
 
-    data = np.loadtxt(args.filename)
+    data = pandas.read_csv(
+        args.filename, delim_whitespace=True, header=0, low_memory=False
+    )
 
-    for i in args.column_index:
-        plt.plot(data[:, args.base_index], data[:, i], label=i)
+    # convert to int if indices are given instead of names
+    base = int(args.base) if args.base.isdigit() else args.base
+    columns = [int(c) if c.isdigit() else c for c in args.columns]
 
-    plt.legend()
+    data.plot(x=base, y=columns)
+
     plt.show()
 
 
