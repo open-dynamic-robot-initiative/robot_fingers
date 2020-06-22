16fb00d370c7589db737549f441f2cf5ec191925
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-06-22 16:22:43 +0200

    Move code for time index matching to separate function
    Use a templated function so it can be used for both the object tracker
    and the camera frontend.

--- include/robot_fingers/trifinger_platform_frontend.hpp
+++ include/robot_fingers/trifinger_platform_frontend.hpp
@@ -57,5 +57,56 @@ private:
     robot_interfaces::TriFingerTypes::Frontend robot_frontend_;
     trifinger_object_tracking::ObjectTrackerFrontend object_tracker_frontend_;
     robot_interfaces::SensorFrontend<CameraObservation> camera_frontend_;
+
+    template <typename FrontendType>
+    time_series::Index find_matching_timeindex(const FrontendType &frontend,
+                                               const time_series::Index t) const
+    {
+        // The given time index t refers to the robot data time series.  To
+        // provide the correct observation from the other frontend for this time
+        // step, find the highest time index t_o of the other frontend where
+        //
+        //     timestamp(t_o) <= timestamp(t)
+        //
+        // Note that this is not always the one that is closest w.r.t. to the
+        // timestamp, i.e.
+        //
+        //     t_o != argmin(|timestamp(t_o) - timestamp(t)|)
+        //
+        // The latter would not be deterministic: the outcome could change when
+        // called twice with the same `t` if a new "other" observation arrived
+        // in between the calls.
+
+        // TODO: The implementation below is very naive.
+        // It simply does a linear search starting from the latest time index.
+        // So worst case performance is O(n) where n is the number of "other"
+        // observations over the period that is covered by the buffer of the
+        // robot data.
+        //
+        // Options to speed this up:
+        // - binary search (?)
+        // - estimate time step size based on last observations
+        // - store matched indices of last call
+        //
+        // Note, however, that `t` is very likely the latest time index in most
+        // cases.  In this case the match for `t_o` will also be the latest
+        // index of the corresponding time series.  In this case, the complexity
+        // is O(1).  So even when implementing a more complex search algorithm,
+        // the first candidate for `t_o` that is checked should always be the
+        // latest one.
+
+        time_series::Timestamp stamp_robot = get_timestamp_ms(t);
+
+        time_series::Index t_other = frontend.get_current_timeindex();
+        time_series::Timestamp stamp_other = frontend.get_timestamp_ms(t_other);
+
+        while (stamp_robot < stamp_other)
+        {
+            t_other--;
+            stamp_other = frontend.get_timestamp_ms(t_other);
+        }
+
+        return t_other;
+    }
 };
 }  // namespace robot_fingers
--- src/trifinger_platform_frontend.cpp
+++ src/trifinger_platform_frontend.cpp
@@ -85,52 +85,7 @@ void TriFingerPlatformFrontend::wait_until_timeindex(
 trifinger_object_tracking::ObjectPose
 TriFingerPlatformFrontend::get_object_pose(const time_series::Index t) const
 {
-    // The given time index t refers to the robot data time series.  To
-    // provide the correct object tracker observation for this time step,
-    // find the highest time index t_o of the object tracker where
-    //
-    //     timestamp(t_o) <= timestamp(t)
-    //
-    // Note that this is not always the one that is closest w.r.t. to the
-    // timestamp, i.e.
-    //
-    //     t_o != argmin(|timestamp(t_o) - timestamp(t)|)
-    //
-    // The latter would not be deterministic: the outcome could change when
-    // called twice with the same `t` if a new object tracker observation
-    // arrived in between the calls.
-
-    // TODO: The implementation below is very naive.
-    // It simply does a linear search starting from the latest time index.
-    // So worst case performance is O(n) where n is the number of object
-    // tracker observations over the period that is covered by the buffer of
-    // the robot data.
-    //
-    // Options to speed this up:
-    // - binary search (?)
-    // - estimate time step size for tracker based on last observations
-    // - store matched indices of last call
-    //
-    // Note, however, that `t` is very likely the latest time index in most
-    // cases.  In this case the match for `t_o` will also be the latest
-    // index of the corresponding time series.  In this case, the complexity
-    // is O(1).  So even when implementing a more complex search algorithm,
-    // the first candidate for `t_o` that is checked should always be the
-    // latest one.
-
-    time_series::Timestamp stamp_robot = get_timestamp_ms(t);
-
-    time_series::Index t_tracker =
-        object_tracker_frontend_.get_current_timeindex();
-    time_series::Timestamp stamp_tracker =
-        object_tracker_frontend_.get_timestamp_ms(t_tracker);
-
-    while (stamp_robot < stamp_tracker)
-    {
-        t_tracker--;
-        stamp_tracker = object_tracker_frontend_.get_timestamp_ms(t_tracker);
-    }
-
+    auto t_tracker = find_matching_timeindex(object_tracker_frontend_, t);
     return object_tracker_frontend_.get_pose(t_tracker);
 }
 
@@ -138,20 +93,7 @@ TriFingerPlatformFrontend::CameraObservation
 TriFingerPlatformFrontend::get_camera_observation(
     const time_series::Index t) const
 {
-    // FIXME redundancy with get_object_pose()
-
-    time_series::Timestamp stamp_robot = get_timestamp_ms(t);
-
-    time_series::Index t_camera = camera_frontend_.get_current_timeindex();
-    time_series::Timestamp stamp_camera =
-        camera_frontend_.get_timestamp_ms(t_camera);
-
-    while (stamp_robot < stamp_camera)
-    {
-        t_camera--;
-        stamp_camera = camera_frontend_.get_timestamp_ms(t_camera);
-    }
-
+    auto t_camera = find_matching_timeindex(camera_frontend_, t);
     return camera_frontend_.get_observation(t_camera);
 }
 
