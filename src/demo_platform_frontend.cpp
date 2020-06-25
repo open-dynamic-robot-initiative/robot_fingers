fc83d36ab5e6e195b355d7d96d3150304fdf886f
Felix Widmaier <luator@users.noreply.github.com>
2020-06-25 10:22:59 +0200

    Combined Frontend for TriFinger, Cameras and Object Tracker (#27)
    Combined `TriFingerPlatformFrontend` which is a wrapper around robot-, camera- and object-tracker-frontend.
    The time index of the robot frontend is used everywhere. Methods related to other frontends internally map this to the corresponding time index of this frontend (see doxygen comments for more details on how this is done).
    
    * Add camera and fake object tracker to trifinger_backend.py
    * Add demo for TriFingerPlatform (can be used with the pybullet backend or trifinger_backend.py)
    * Use catkin_add_pybind11_module

---
+++ src/demo_platform_frontend.cpp
@@ -0,0 +1,62 @@
+#include <opencv2/highgui.hpp>
+#include <robot_fingers/trifinger_platform_frontend.hpp>
+
+using namespace robot_fingers;
+
+int main()
+{
+    auto frontend = TriFingerPlatformFrontend();
+
+    TriFingerPlatformFrontend::Action::Vector position;
+
+    position << -0.2, -0.9, -1.7, -0.2, -0.9, -1.7, -0.2, -0.9, -1.7;
+
+    while (true)
+    {
+        time_series::Index t;
+
+        position[0] *= -1;
+        position[3] *= -1;
+        position[6] *= -1;
+
+        auto action = TriFingerPlatformFrontend::Action::Position(position);
+
+        for (int i = 0; i < 300; i++)
+        {
+            t = frontend.append_desired_action(action);
+            frontend.wait_until_timeindex(t);
+        }
+
+        auto robot_observation = frontend.get_robot_observation(t);
+        std::cout << "Robot[:3]: " << robot_observation.position[0] << ", "
+                  << robot_observation.position[1] << ", "
+                  << robot_observation.position[2] << std::endl;
+
+        try
+        {
+            auto object_pose = frontend.get_object_pose(t);
+            std::cout << "Object: " << object_pose.position[0] << ", "
+                      << object_pose.position[0] << ", "
+                      << object_pose.position[0] << std::endl;
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "No object pose: " << e.what() << std::endl;
+        }
+
+        try
+        {
+            auto images = frontend.get_camera_observation(t);
+            cv::imshow("camera60", images.cameras[0].image);
+            cv::imshow("camera180", images.cameras[1].image);
+            cv::imshow("camera300", images.cameras[2].image);
+            cv::waitKey(3);
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "No camera image: " << e.what() << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+}
---
+++ src/trifinger_platform_frontend.cpp
@@ -0,0 +1,100 @@
+/**
+ * @file
+ * @brief Combined frontend for the TriFinger Platform
+ * @copyright 2020, Max Planck Gesellschaft.  All rights reserved.
+ * @license BSD 3-clause
+ */
+#include <robot_fingers/trifinger_platform_frontend.hpp>
+
+namespace robot_fingers
+{
+TriFingerPlatformFrontend::TriFingerPlatformFrontend(
+    robot_interfaces::TriFingerTypes::BaseDataPtr robot_data,
+    trifinger_object_tracking::ObjectTrackerData::Ptr object_tracker_data,
+    std::shared_ptr<robot_interfaces::SensorData<CameraObservation>>
+        camera_data)
+    : robot_frontend_(robot_data),
+      object_tracker_frontend_(object_tracker_data),
+      camera_frontend_(camera_data)
+{
+}
+
+TriFingerPlatformFrontend::TriFingerPlatformFrontend()
+    : robot_frontend_(
+          std::make_shared<robot_interfaces::TriFingerTypes::MultiProcessData>(
+              "trifinger", false)),
+      object_tracker_frontend_(
+          std::make_shared<trifinger_object_tracking::ObjectTrackerData>(
+              "object_tracker", false)),
+      camera_frontend_(
+          std::make_shared<
+              robot_interfaces::MultiProcessSensorData<CameraObservation>>(
+              "tricamera", false, 100))
+
+{
+}
+
+time_series::Index TriFingerPlatformFrontend::append_desired_action(
+    const Action &desired_action)
+{
+    return robot_frontend_.append_desired_action(desired_action);
+}
+
+TriFingerPlatformFrontend::RobotObservation
+TriFingerPlatformFrontend::get_robot_observation(
+    const time_series::Index &t) const
+{
+    return robot_frontend_.get_observation(t);
+}
+
+TriFingerPlatformFrontend::Action TriFingerPlatformFrontend::get_desired_action(
+    const time_series::Index &t) const
+{
+    return robot_frontend_.get_desired_action(t);
+}
+
+TriFingerPlatformFrontend::Action TriFingerPlatformFrontend::get_applied_action(
+    const time_series::Index &t) const
+{
+    return robot_frontend_.get_applied_action(t);
+}
+
+TriFingerPlatformFrontend::RobotStatus
+TriFingerPlatformFrontend::get_robot_status(const time_series::Index &t) const
+{
+    return robot_frontend_.get_status(t);
+}
+
+time_series::Timestamp TriFingerPlatformFrontend::get_timestamp_ms(
+    const time_series::Index &t) const
+{
+    return robot_frontend_.get_timestamp_ms(t);
+}
+
+time_series::Index TriFingerPlatformFrontend::get_current_timeindex() const
+{
+    return robot_frontend_.get_current_timeindex();
+}
+
+void TriFingerPlatformFrontend::wait_until_timeindex(
+    const time_series::Index &t) const
+{
+    robot_frontend_.wait_until_timeindex(t);
+}
+
+trifinger_object_tracking::ObjectPose
+TriFingerPlatformFrontend::get_object_pose(const time_series::Index t) const
+{
+    auto t_tracker = find_matching_timeindex(object_tracker_frontend_, t);
+    return object_tracker_frontend_.get_pose(t_tracker);
+}
+
+TriFingerPlatformFrontend::CameraObservation
+TriFingerPlatformFrontend::get_camera_observation(
+    const time_series::Index t) const
+{
+    auto t_camera = find_matching_timeindex(camera_frontend_, t);
+    return camera_frontend_.get_observation(t_camera);
+}
+
+}  // namespace robot_fingers
