1e9efb60617367c596a07fc8e4689713de3bef23
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-07-14 16:11:10 +0200

    Refactor robot creation by name
    Move the dictionary with default setups to the Robot class to have it in
    a central place instead of duplicating it in each script.

--- python/robot_fingers/__init__.py
+++ python/robot_fingers/__init__.py
@@ -1,6 +1,6 @@
-from robot_fingers.robot import Robot, demo_print_position
-
 from robot_fingers.py_real_finger import *
 from robot_fingers.py_trifinger import *
 from robot_fingers.py_one_joint import *
 from robot_fingers.py_two_joint import *
+
+from robot_fingers.robot import Robot, demo_print_position
