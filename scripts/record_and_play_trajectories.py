2376ce89021b8ab8ce9e724289439c315a1b4ff5
mwuethri <manuel.wuthrich@gmail.com>
2020-07-27 15:15:17 +0200

    added a script which records and replays trajectories (#41)
    * added a script which records and replays trajectories
    
    * moved robot creation into main

---
+++ scripts/record_and_play_trajectories.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Record data while the user is manually moving the robot."""
+import argparse
+import curses
+import numpy as np
+import robot_fingers
+
+
+def record(trajectory):
+    trajectory[:] = []
+    t = 0
+    while True:
+        t = robot.frontend.append_desired_action(robot.Action())
+        robot.frontend.wait_until_timeindex(t)
+        trajectory += [robot.frontend.get_observation(t).position]
+
+        if len(trajectory) > 3000:
+            if np.abs(trajectory[-1] - trajectory[-2000]).sum() < 0.01:
+                return
+
+
+def play(trajectory, factor):
+    for position in trajectory[::factor]:
+        action = robot.Action(position=position)
+        t = robot.frontend.append_desired_action(action)
+        robot.frontend.wait_until_timeindex(t - 10)
+
+    robot.frontend.append_desired_action(robot.Action())
+
+
+if __name__ == "__main__":
+    robot = robot_fingers.Robot.create_by_name('trifingerpro')
+    robot.initialize()
+
+    trajectory = []
+    while True:
+        print('enter key')
+        key = input()
+        if key == 'r':
+            print('recording')
+            record(trajectory)
+        elif key in list(map(str, range(1, 20))):
+            print('playing with factor ' + key)
+            play(trajectory, int(key))
+        else:
+            print('invalid key')
