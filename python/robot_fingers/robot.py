6f276ba51c02d4e39252fda7901f37b75e018fac
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-08-18 14:00:53 +0200

    Add support for absolute paths to config files
    If the given config files starts with "/", it is considered to be an
    absolute path and is used as is.  Otherwise it is searched in
    robot_fingers/config just like before.

--- python/robot_fingers/robot.py
+++ python/robot_fingers/robot.py
@@ -89,18 +89,24 @@ class Robot:
         :param robot_module: The module that defines the robot classes (i.e.
             `Data`, `Frontend`, `Backend`, ...)
         :param create_backend_function: Function to create the robot backend.
-        :param config_file_name: Name of the config file (expected to be
-            located in robot_fingers/config).
+        :param config_file_name: Either an absolute path to a config file
+            (needs to start with "/" in this case) or the name of a config
+            file located in robot_fingers/config.
         """
         # convenience mapping of the Action type
         self.Action = robot_module.Action
 
-        # Use the default config file from the robot_fingers package
-        config_file_path = os.path.join(
-            rospkg.RosPack().get_path("robot_fingers"),
-            "config",
-            config_file_name,
-        )
+        # If config_file_name starts with "/" assume it is an absolute path and
+        # use it as is.  Otherwise interpret it relative to the config
+        # directory of the robot_fingers package.
+        if config_file_name.startswith("/"):
+            config_file_path = config_file_name
+        else:
+            config_file_path = os.path.join(
+                rospkg.RosPack().get_path("robot_fingers"),
+                "config",
+                config_file_name,
+            )
 
         # Storage for all observations, actions, etc.
         self.robot_data = robot_module.SingleProcessData()
