5cf25a4432acf5f1c80c374901ec164a6a684d3d
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-08-21 16:23:14 +0200

    Add arguments for logging and episode length to trifinger_backend.py

--- scripts/trifinger_backend.py
+++ scripts/trifinger_backend.py
@@ -12,6 +12,15 @@ import robot_fingers
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
+        "--max-number-of-actions",
+        "-a",
+        type=int,
+        required=True,
+        help="""Maximum numbers of actions that are processed.  After this the
+            backend shuts down automatically.
+        """,
+    )
+    parser.add_argument(
         "--cameras",
         "-c",
         action="store_true",
@@ -22,6 +31,20 @@ def main():
         action="store_true",
         help="Run fake object tracker backend",
     )
+    parser.add_argument(
+        "--robot-logfile",
+        type=str,
+        help="""Path to a file to which the robot data log is written.  If not
+            specified, no log is generated.
+        """,
+    )
+    parser.add_argument(
+        "--camera-logfile",
+        type=str,
+        help="""Path to a file to which the camera data is written.  If not
+            specified, no log is generated.
+        """,
+    )
     args = parser.parse_args()
 
     # Use the default config file from the robot_fingers package
@@ -31,6 +54,9 @@ def main():
 
     # Storage for all observations, actions, etc.
     robot_data = robot_interfaces.trifinger.MultiProcessData("trifinger", True)
+
+    if args.robot_logfile:
+        logger = robot_interfaces.trifinger.Logger(robot_data)
 
     # The backend sends actions from the data to the robot and writes
     # observations from the robot to the data.
@@ -44,26 +70,52 @@ def main():
     if args.cameras:
         import trifinger_cameras
 
+        CAMERA_TIME_SERIES_LENGTH = 100
+
         camera_data = trifinger_cameras.tricamera.MultiProcessData(
-            "tricamera", True, 100
+            "tricamera", True, CAMERA_TIME_SERIES_LENGTH
         )
         camera_driver = trifinger_cameras.tricamera.TriCameraDriver(
             "camera60", "camera180", "camera300"
         )
-        camera_backend = trifinger_cameras.tricamera.Backend(
+        camera_backend = trifinger_cameras.tricamera.Backend(  # noqa
             camera_driver, camera_data
         )
+
+        if args.camera_logfile:
+            camera_fps = 100
+            robot_rate_hz = 1000
+            episode_length_s = args.max_number_of_actions / robot_rate_hz
+            # Compute camera log size based on number of robot actions plus a
+            # 10% buffer
+            log_size = camera_fps * episode_length_s * 1.1
+
+            print("Start camera logger with buffer size", log_size)
+            logger = trifinger_cameras.tricamera.Logger(camera_data, log_size)
+            logger.start()
 
     if args.fake_object_tracker:
         import trifinger_object_tracking.py_object_tracker as object_tracker
         object_tracker_data = object_tracker.Data("object_tracker", True)
-        object_tracker_backend = object_tracker.FakeBackend(
+        object_tracker_backend = object_tracker.FakeBackend(  # noqa
             object_tracker_data
         )
 
+    backend.wait_until_terminated()
 
-    while backend.wait_until_terminated():
-        pass
+    if args.record:
+        print("Save recorded camera data to file {}".format(args.camera_logfile))
+        logger.stop_and_save(args.camera_logfile)
+
+    if args.robot_logfile:
+        print("Save robot data to file {}".format(args.robot_logfile))
+        if args.max_number_of_actions:
+            end_index = args.max_number_of_actions
+        else:
+            end_index = -1
+        logger.write_current_buffer(
+            args.robot_logfile, start_index=0, end_index=end_index
+        )
 
 
 if __name__ == "__main__":
