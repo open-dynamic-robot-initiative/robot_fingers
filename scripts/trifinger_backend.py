9475b51d7bfa38833c91abd022163a79ef6d04dd
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-08-26 09:18:39 +0200

    backend: Add "ready indicator" option
    If set, the specified file will be created once the backend is ready
    with initialization and will be deleted when it is shut down.  This can
    be used by the submission system runscript to see the current state.

--- scripts/trifinger_backend.py
+++ scripts/trifinger_backend.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Run TriFinger back-end using multi-process robot data."""
 import argparse
-import os
-
-import rospkg
+import pathlib
 
 import robot_interfaces
 import robot_fingers
@@ -43,6 +41,14 @@ def main():
         type=str,
         help="""Path to a file to which the camera data is written.  If not
             specified, no log is generated.
+        """,
+    )
+    parser.add_argument(
+        "--ready-indicator",
+        type=str,
+        metavar="READY_INDICATOR_FILE",
+        help="""Path to a file that will be created once the backend is ready
+            and will be deleted again when it stops (before storing the logs).
         """,
     )
     args = parser.parse_args()
@@ -105,13 +111,23 @@ def main():
 
         print("Initialize camera logger with buffer size", log_size)
         camera_logger = trifinger_cameras.tricamera.Logger(camera_data, log_size)
+
+    # if specified, create the "ready indicator" file to indicate that the
+    # backend is ready
+    if args.ready_indicator:
+        pathlib.Path(args.ready_indicator).touch()
+
+    if args.cameras and args.camera_logfile:
         backend.wait_until_first_action()
         camera_logger.start()
-        print("Start camera logging", log_size)
-
-
+        print("Start camera logging")
 
     backend.wait_until_terminated()
+
+    # delete the ready indicator file to indicate that the backend has shut
+    # down
+    if args.ready_indicator:
+        pathlib.Path(args.ready_indicator).unlink()
 
     if args.cameras and args.camera_logfile:
         print("Save recorded camera data to file {}".format(args.camera_logfile))
