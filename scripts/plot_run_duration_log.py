dbb7fc6be770e50e67043ba03e7ace160b194d0e
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-03-30 11:01:36 +0200

    Add script to plot run duration log

---
+++ scripts/plot_run_duration_log.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Plot the data of a given run duration log file."""
+import argparse
+import datetime
+import pathlib
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "logfile", type=pathlib.Path, help="Path to the logfile."
+    )
+    args = parser.parse_args()
+
+    data = np.loadtxt(args.logfile)
+
+    timestamps = [datetime.datetime.fromtimestamp(t) for t in data[:, 0]]
+    cumsum = data[:, 1].cumsum()
+
+    fig, ax = plt.subplots(1, 1)
+
+    ax.fill_between(timestamps, cumsum)
+
+    ax.set_title(args.logfile.stem)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Accumulated Robot Run Duration [steps]")
+
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m-%d"))
+
+    plt.show()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
