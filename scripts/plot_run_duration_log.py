2f3f86b53ddeb16944ce7e3f0597acb28517847f
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-05-31 17:25:44 +0200

    Run duration log: show duration in hours

--- scripts/plot_run_duration_log.py
+++ scripts/plot_run_duration_log.py
@@ -22,15 +22,27 @@ def main():
     timestamps = [datetime.datetime.fromtimestamp(t) for t in data[:, 0]]
     cumsum = data[:, 1].cumsum()
 
+    # rescale from steps to hours (assuming 1 step == 1 ms)
+    cumsum /= 3600000
+
     fig, ax = plt.subplots(1, 1)
 
     ax.fill_between(timestamps, cumsum)
 
     ax.set_title(args.logfile.stem)
     ax.set_xlabel("Date")
-    ax.set_ylabel("Accumulated Robot Run Duration [steps]")
+    ax.set_ylabel("Accumulated Robot Run Duration [h]")
 
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m-%d"))
+
+    ax.text(
+        0.05,
+        0.95,
+        "total run duration: %.1f h" % cumsum[-1],
+        transform=ax.transAxes,
+        verticalalignment="top",
+        bbox=dict(boxstyle="round", alpha=0.5),
+    )
 
     plt.show()
 
