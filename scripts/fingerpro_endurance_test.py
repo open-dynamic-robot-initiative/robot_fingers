6cf7442f812b24ca62e3416076e8f5eb5c1effe8
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-07-01 15:20:15 +0200

    Add "push against obstacle" sequence to endurance test
    In full-range-mode, the finger pushes against an obstacle once every 10
    iterations.

--- scripts/fingerpro_endurance_test.py
+++ scripts/fingerpro_endurance_test.py
@@ -68,19 +68,111 @@ def move_with_random_positions():
         time_printer.update()
 
 
-def move_on_full_range():
+def execute_trajectory(frontend, positions, timeout=4000, tolerance=0.03):
+    """Move joints open loop on a trajectory.
+
+    Args:
+        frontend: Robot frontend for sending commands.
+        positions: List of joint positions.
+        timeout: Maximum number of steps to pursue one step.
+        tolerance: Position tolerance within a target position is considered to
+            be reached.
+    """
+    for target in positions:
+        action = robot_interfaces.finger.Action(position=target)
+        for _ in range(timeout):
+            t = frontend.append_desired_action(action)
+            obs = frontend.get_observation(t)
+
+            dist = action.position - obs.position
+
+            # stop as soon as the goal is reached
+            if np.all(np.linalg.norm(dist) < tolerance):
+                break
+
+
+def push_block_sequence(frontend):
+    """Push against the block at the center once from each side.
+
+    The block is expected to be placed exactly below the middle joint, so that
+    the finger tip can reach it.
+    """
+
+    # first element is trajectory to bring robot into position, second element
+    # is push command (which should be held longer)
+    sequence = [
+        ([(0, 0, 0)], (0.5, 0, 0)),
+        (
+            [
+                (0, 0, 0),
+                (0, 0.9, -1.7),
+                (0.4, 0.9, -1.7),
+                (0.4, 0, 0),
+            ],
+            (0, 0, 0),
+        ),
+        (
+            [
+                (0.5, 0, 0),
+                (0.5, 0.9, -1.7),
+                (0.5, 0.9, 0),
+                (0.27, 0.5, -0.75),
+            ],
+            (0.27, 0.25, -0.75),
+        ),
+        (
+            [
+                (0.27, 0.5, -0.75),
+                (0.27, 0.8, -0.75),
+                (0.27, 0.8, -2),
+                (0.27, -0.5, -2),
+                (0.27, -0.5, 0.7),
+            ],
+            (0.27, -0.25, 0.7),
+        ),
+    ]
+
+    go_back = [
+        (0.27, -0.5, 0.7),
+        (0.27, -0.5, -2),
+        (0.27, 0.8, -2),
+    ]
+
+    for goto_traj, push in sequence:
+        execute_trajectory(frontend, goto_traj)
+
+        # wait a moment
+        for _ in range(600):
+            action = robot_interfaces.finger.Action(position=goto_traj[-1])
+            t = frontend.append_desired_action(action)
+            frontend.wait_until_timeindex(t)
+
+        # push!
+        for _ in range(6000):
+            action = robot_interfaces.finger.Action(position=push)
+            t = frontend.append_desired_action(action)
+            frontend.wait_until_timeindex(t)
+
+    execute_trajectory(frontend, go_back)
+
+
+def move_on_full_range(push_interval):
     target_positions = [
         [0, 0, 0],
         [0, 1.57, -1.57],
-        [0, 2.2, -2.5],
+        [0, 2.4, -2.5],
         [2, 1.57, -2.5],
         [-1.57, 1.57, -2.5],
         [0.5, 1.57, -2.5],
         [0.5, -3, 2.5],
+        [0, 0, 0],
     ]
-    tolerance = 0.03
 
-    (robot_type, backend_func, config_file) = robot_fingers.robot.robot_configs["fingerpro"]
+    (
+        robot_type,
+        backend_func,
+        config_file,
+    ) = robot_fingers.robot.robot_configs["fingerpro"]
     config_file_path = robot_fingers.robot.get_config_dir() / config_file
 
     n_joints = 3
@@ -93,7 +185,9 @@ def move_on_full_range():
     config.soft_position_limits_upper = [np.inf] * n_joints
 
     robot_data = robot_interfaces.finger.SingleProcessData()
-    robot_backend = robot_fingers.create_real_finger_backend(robot_data, config)
+    robot_backend = robot_fingers.create_real_finger_backend(
+        robot_data, config
+    )
     robot_frontend = robot_interfaces.finger.Frontend(robot_data)
 
     robot_backend.initialize()
@@ -102,24 +196,18 @@ def move_on_full_range():
 
     time_printer = robot_fingers.utils.TimePrinter()
 
-    idx = 0
+    i = 0
     while True:
-        action = robot_interfaces.finger.Action(position=target_positions[idx])
-        for _ in range(4000):
-            t = robot_frontend.append_desired_action(action)
-            obs = robot_frontend.get_observation(t)
+        execute_trajectory(robot_frontend, target_positions)
 
-            dist = action.position - obs.position
-
-            # stop as soon as the goal is reached
-            if np.all(np.linalg.norm(dist) < tolerance):
-                break
+        if i % push_interval == 0:
+            push_block_sequence(robot_frontend)
 
         # print current date/time every hour, so we can roughly see how long it
         # ran in case it crashes during a long-run-test.
         time_printer.update()
 
-        idx = (idx + 1) % len(target_positions)
+        i += 1
 
 
 def main():
@@ -131,12 +219,22 @@ def main():
             using the full mechanical range.
         """,
     )
+    parser.add_argument(
+        "--push-interval",
+        type=int,
+        metavar="N",
+        default=10,
+        help="""[Only if --full-range is set]  Perform a push sequence every N
+            iterations.  Default is %(default)d.
+        """,
+    )
     args = parser.parse_args()
 
     if args.full_range:
-        move_on_full_range()
+        move_on_full_range(push_interval=args.push_interval)
     else:
         move_with_random_positions()
+
 
 if __name__ == "__main__":
     main()
