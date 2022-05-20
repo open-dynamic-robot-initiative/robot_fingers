1df00c6bf6f894a3f9f89a54e1b52ffaf4401a43
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-05-20 09:37:01 +0200

    Explicitly list all imports in __init__.py
    The `import *` causes trouble for both mypy and flake8, so don't use it.

--- python/robot_fingers/__init__.py
+++ python/robot_fingers/__init__.py
@@ -1,9 +1,42 @@
 from . import utils
 
-from .py_real_finger import *
-from .py_trifinger import *
-from .py_one_joint import *
-from .py_two_joint import *
-from .py_solo_eight import *
+from .py_real_finger import (
+    create_real_finger_backend,
+    create_fake_finger_backend,
+    FingerConfig,
+)
+from .py_trifinger import (
+    create_trifinger_backend,
+    TriFingerConfig,
+    TriFingerPlatformFrontend,
+    TriFingerPlatformWithObjectFrontend,
+    TriFingerPlatformLog,
+    TriFingerPlatformWithObjectLog,
+)
+from .py_one_joint import create_one_joint_backend, OneJointConfig
+from .py_two_joint import create_two_joint_backend, TwoJointConfig
+from .py_solo_eight import create_solo_eight_backend, SoloEightConfig
 
 from .robot import Robot, demo_print_position
+
+
+__all__ = (
+    "utils",
+    "create_real_finger_backend",
+    "create_fake_finger_backend",
+    "FingerConfig",
+    "create_trifinger_backend",
+    "TriFingerConfig",
+    "TriFingerPlatformFrontend",
+    "TriFingerPlatformWithObjectFrontend",
+    "TriFingerPlatformLog",
+    "TriFingerPlatformWithObjectLog",
+    "create_one_joint_backend",
+    "OneJointConfig",
+    "create_two_joint_backend",
+    "TwoJointConfig",
+    "create_solo_eight_backend",
+    "SoloEightConfig",
+    "Robot",
+    "demo_print_position",
+)
