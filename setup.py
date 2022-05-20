81ae5887f8a065c76e953de666510eef5f4e87f6
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-05-20 09:47:21 +0200

    Remove obsolete setup.py
    This is a remainder from when this was a catkin package.

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=["robot_fingers"], package_dir={"": "python"}
-)
-
-setup(**d)
