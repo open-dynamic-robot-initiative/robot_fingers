0c5b69667cd497c2510af1ae8a3cb7d83199b96f
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-05-20 09:40:46 +0200

    Add type hints to record_and_play_trajectories.py

--- scripts/record_and_play_trajectories.py
+++ scripts/record_and_play_trajectories.py
@@ -4,9 +4,10 @@ import argparse
 import curses
 import numpy as np
 import robot_fingers
+import typing
 
 
-def record(trajectory):
+def record(trajectory: typing.List[np.ndarray]):
     trajectory[:] = []
     t = 0
     while True:
@@ -19,7 +20,7 @@ def record(trajectory):
                 return
 
 
-def play(trajectory, factor):
+def play(trajectory: typing.List[np.ndarray], factor: int):
     for position in trajectory[::factor]:
         action = robot.Action(position=position)
         t = robot.frontend.append_desired_action(action)
@@ -32,7 +33,7 @@ if __name__ == "__main__":
     robot = robot_fingers.Robot.create_by_name("trifingerpro")
     robot.initialize()
 
-    trajectory = []
+    trajectory: typing.List[np.ndarray] = []
     while True:
         print("enter key")
         key = input()
